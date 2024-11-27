25eb549bde1a9010e364b4d2a3cc24ab2b569ba5
karkman <antti.karkman@helsinki.fi>
2024-11-27 13:42:45 +0200

    updates

--- src/metaphlan.sh
+++ src/metaphlan.sh
@@ -7,22 +7,22 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 12
 #SBATCH --mem 20G
-#SBATCH --account project_XXXXXXX
+#SBATCH --account project_2012151
 #SBATCH --array=1-9
 #SBATCH --gres=nvme:100
 
-module load metaphlan/4.0.6
+module load metaphlan/4.1.1
 mkdir -p 00_LOGS
 mkdir -p 05_TAXONOMY
 
 SAMPLE_ACC=$(sed -n ${SLURM_ARRAY_TASK_ID}p 01_DATA/DF16_accessions.txt)
 
 metaphlan \
-    01_DATA/${SAMPLE_ACC}_1.fastq.gz,01_DATA/${SAMPLE_ACC}_2.fastq.gz \
+    01_DATA/${SAMPLE_ACC}_1.fastq.gz \
     --nproc $SLURM_CPUS_PER_TASK \
     --unclassified_estimation \
     --sample_id ${SAMPLE_ACC} \
     --input_type fastq \
-    --bowtie2db /scratch/project_XXXXXXX/DBs/metaphlan \
+    --bowtie2db /scratch/project_2012151/DBs/metaphlan \
     --output 05_TAXONOMY/${SAMPLE_ACC}.txt \
     --bowtie2out 05_TAXONOMY/${SAMPLE_ACC}.bowtie2.bz2 
