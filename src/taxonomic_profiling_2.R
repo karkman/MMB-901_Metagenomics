893e72a876265bda0b72c77cc018b5513076df0a
karkman <antti.karkman@helsinki.fi>
2025-11-20 21:02:09 +0200

    new R code

---
+++ src/taxonomic_profiling_2.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(phyloseq)
+library(microViz)
+library(mia)
+
+## setwd() sets the working directory. 
+## Set it to your course directory. Use the full path and put it between the quotes.
+setwd("")
+
+## Read in the metadata file
+metadata <- read.table("doc/metadata.txt", header = TRUE, row.names = 1)
+
+## Read in the metaphlan file as a TreeSummarizedExperiment object
+tse <- mia::importMetaPhlAn("metaphlan.txt")
+
+## Save the common samples
+common.samples <- intersect(colnames(tse), rownames(metadata))
+
+## Add metadata to the tse usingcommon sample names
+colData(tse) <- S4Vectors::DataFrame(metadata[common.samples,])
+
+## Check the tse structure
+tse
+
+## Covert to phyloseq object
+
+phy <- convertToPhyloseq(tse)
+
+## Then the phyloseq object is ready and we can visualise the results using ord_explore function from microViz package
+phy %>% ord_explore()
