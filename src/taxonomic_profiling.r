083d925f939aae66de3b53426764de5958b7e248
karkman <antti.karkman@helsinki.fi>
2025-11-20 21:33:50 +0200

    tax profiling changed

--- src/taxonomic_profiling.r
+++ src/taxonomic_profiling.r
@@ -1,8 +1,7 @@
-
 ## Some setup for R packages needed for the analysis
 .libPaths(c("/projappl/project_2016640/project_rpackages_r451", .libPaths()))
-libpath <- .libPaths()[1]
 
+# Load required libraries
 library(tidyverse)
 library(phyloseq)
 library(microViz)
@@ -12,26 +11,25 @@ library(mia)
 ## Set it to your course directory. Use the full path and put it between the quotes.
 setwd("")
 
-## read in the data
-## First we take the taxonomy from the table and store it as TAX
-TAX <- read_delim("05_TAXONOMY/metaphlan_species.txt", delim="\t",  comment="#") %>%
-  dplyr::select(clade_name) %>%
-  separate(clade_name, sep="\\|", into=c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"))
+## Read in the metadata file
+metadata <- read.table("doc/metadata.txt", header = TRUE, row.names = 1)
 
-## Then we read the same table, but this time we save the relative abundances for each taxa and each sample as OTU
-OTU <- read_delim("05_TAXONOMY/metaphlan_species.txt", delim="\t",  comment="#") %>%  
-  dplyr::select(-clade_name)
+## Read in the metaphlan file as a TreeSummarizedExperiment object
+tse <- mia::importMetaPhlAn("05_TAXONOMY/metaphlan.txt", assay.type="counts")
 
-## We do some tidying to our tables  
-OTU <- OTU %>% mutate(rowname=paste0("tax", seq(1:nrow(OTU)))) %>% column_to_rownames("rowname")
-TAX <- TAX %>% mutate(rowname=paste0("tax", seq(1:nrow(TAX)))) %>% column_to_rownames("rowname")
+## Save the common samples
+common.samples <- intersect(colnames(tse), rownames(metadata))
 
-## And finally convert them to phyloseq object
-DF16_ps <- phyloseq(otu_table(OTU, taxa_are_rows=TRUE), tax_table(as.matrix(TAX)))
+## Add metadata to the tse usingcommon sample names
+colData(tse) <- S4Vectors::DataFrame(metadata[common.samples,])
 
-## As a last step we read in a table having some metadata for each of the samples and attach it to our phyloseq object
-metadata <- read.table("doc/metadata.txt", row.names = 1, header=TRUE)
-sample_data(DF16_ps) <- sample_data(metadata)
+## Check the tse structure
+tse
 
-## Then the phyloseq object is ready and we can visualise the results using ord_explore function from microViz package
-DF16_ps %>% ord_explore()
+## Covert to phyloseq object
+phy <- convertToPhyloseq(tse)
+
+## Then the phyloseq object is ready and
+## we can visualise the results using
+## the ord_explore function from microViz package
+phy %>% ord_explore()
